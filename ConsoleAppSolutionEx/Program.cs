c0537f9bd11770edc16761da6efb0ac1264431c0
Ulf Bengtsson <ulf.bengtsson@lexicon.se>
2021-03-09 16:02:18 +0100

    Add project files.

---
+++ ConsoleAppSolutionEx/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ConsoleAppSolutionEx
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
